5d64b31a9d8302a2c471553831f7ae4927709e8a
Uttam Mishra <mishrauttam526@gmail.com>
2024-09-24 12:25:01 +0530

    "URL Updated"

--- server/controllers/resetPassword.js
+++ server/controllers/resetPassword.js
@@ -25,8 +25,7 @@ exports.resetPasswordToken = async (req, res) => {
     console.log("DETAILS", updatedDetails)
 
     // const url = `http://localhost:3000/update-password/${token}`
-    const BASE_URL=process.env.REACT_APP_BASE_URL;
-    const url = `${BASE_URL}/update-password/${token}`
+    const url = `https://codeway-frontend-kohl.vercel.app/update-password/${token}`
 
     await mailSender(
       email,
